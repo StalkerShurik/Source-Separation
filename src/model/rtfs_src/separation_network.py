68d46c8c68c3d5f83d2ed77d186b80dd83469a0f
Alexander Gaponov <agaponov04@gmail.com.com>
2024-11-17 00:05:55 +0300

    CAF

--- src/model/rtfs_src/separation_network.py
+++ src/model/rtfs_src/separation_network.py
@@ -1,19 +1,70 @@
 import torch
 import torch.nn as nn
+from conv_blocks import ConvBlockWithActivation
+from rtfs_block import RTFSBlock
 
-from .rtfs_block import RTFSBlock
 
+class CAF(nn.Module):
+    def __init__(self, audio_channels: int, video_channels: int, n_heads=8):
+        super(CAF, self).__init__()
 
-class MultiModalBlock(nn.Module):
-    def __init__(self, audio_channels: int, video_channels: int):
-        super(MultiModalBlock, self).__init__()
-        # TODO
+        self.nheads = n_heads
+
+        self.P1 = ConvBlockWithActivation(
+            in_channels=audio_channels,
+            out_channels=audio_channels,
+            kernel_size=1,
+            is_conv_2d=True,
+            groups=audio_channels,
+            activation_function=torch.nn.Identity,
+        )
+        self.P2 = ConvBlockWithActivation(
+            in_channels=audio_channels,
+            out_channels=audio_channels,
+            kernel_size=1,
+            is_conv_2d=True,
+            groups=audio_channels,
+        )
+
+        self.F1 = ConvBlockWithActivation(
+            in_channels=video_channels,
+            out_channels=audio_channels * n_heads,
+            kernel_size=1,
+            is_conv_2d=False,
+            groups=audio_channels,
+            activation_function=torch.nn.Identity,
+        )
+        self.F2 = ConvBlockWithActivation(
+            in_channels=video_channels,
+            out_channels=audio_channels,
+            kernel_size=1,
+            is_conv_2d=False,
+            groups=audio_channels,
+            activation_function=torch.nn.Identity,
+        )
 
     def forward(
         self, audio_features: torch.Tensor, video_features: torch.Tensor
     ) -> torch.Tensor:
-        # TODO
-        pass
+        """
+        audio_features: B x Ca x Ta x F
+        video_features: B x Cv x Ta
+        """
+
+        B, Ca, Ta, F = audio_features.shape
+
+        audio_value = self.P1(audio_features)  # B x Ca x Ta x F
+        audio_gate = self.P2(audio_features)  # B x Ca x Ta x F
+
+        video_attn = self.F1(video_features)
+        video_attn = video_attn.reshape(B, Ca, self.nheads, -1).mean(dim=2)
+        video_attn = torch.softmax(video_attn, -1)
+        video_attn = torch.nn.functional.interpolate(video_attn, size=Ta)
+
+        video_key = self.F2(video_features)
+        video_key = torch.nn.functional.interpolate(video_key, size=Ta)
+
+        return video_key * audio_gate + video_attn * audio_value
 
 
 class SeparationNetwork(nn.Module):
@@ -34,18 +85,19 @@ class SeparationNetwork(nn.Module):
 
         self.audio_repeats = audio_repeats
 
-        self.crossmodal_fusion = MultiModalBlock()  # TODO
+        self.CAE = CAF(
+            audio_channels, video_channels
+        )  # CHECK IF IT IS APPROPRIATE CHANNELS
 
     def forward(
         self, audio_features: torch.Tensor, video_features: torch.Tensor
     ) -> torch.Tensor:
         audio_features_residual = audio_features
 
-        # cross modal fusion
         audio_features = self.audio_network(audio_features)
         video_features = self.video_network(video_features)
 
-        audio_features = self.crossmodal_fusion(
+        audio_features = self.CAE(
             audio_features=audio_features, video_features=video_features
         )
 
