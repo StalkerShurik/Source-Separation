638ecb3dd18a047116cab6cef2f9e3ba1771174f
Alexander Gaponov <agaponov04@gmail.com.com>
2024-11-17 11:40:54 +0300

    audio encode fix

--- src/model/rtfs_src/audio_encoder.py
+++ src/model/rtfs_src/audio_encoder.py
@@ -5,9 +5,8 @@ import torch.nn as nn
 class RTFS_AudioEncoder(nn.Module):
     def __init__(
         self,
-        n_fft: int,
         hop_length: int = 128,
-        win_length: int = 256,
+        features: int = 256,
         output_channels: int = 256,
         kernel_size: int = 3,
         *args,
@@ -15,9 +14,8 @@ class RTFS_AudioEncoder(nn.Module):
     ) -> None:
         super().__init__(*args, **kwargs)
 
-        self.n_fft: int = n_fft
         self.hop_length: int = hop_length
-        self.win_length: int = win_length
+        self.features: int = features
 
         self.layers: nn.Module = nn.Sequential(
             nn.Conv2d(
@@ -31,13 +29,12 @@ class RTFS_AudioEncoder(nn.Module):
             # TODO: try to use Global normalization
             nn.BatchNorm2d(output_channels),
             nn.ReLU(),
-            nn.Conv2d(
-                in_channels=output_channels,
-                out_channels=output_channels,
-                kernel_size=1,
-                stride=1,
-            ),
-            # TODO: try to add normalization here too
+            # nn.Conv2d(
+            #     in_channels=output_channels,
+            #     out_channels=output_channels,
+            #     kernel_size=1,
+            #     stride=1,
+            # ),
         )
 
     def forward(self, raw_audio: torch.Tensor) -> torch.Tensor:
@@ -45,15 +42,19 @@ class RTFS_AudioEncoder(nn.Module):
         Input: tensor with audio seqs: B x L
         Output: B x C x T x F
         """
-
         x_spectr = torch.stft(
             raw_audio,
-            n_fft=self.n_fft,
+            n_fft=self.features,
             hop_length=self.hop_length,
-            win_length=self.win_length,
+            win_length=self.features,
+            window=torch.hann_window(window_length=self.features),
             return_complex=True,
         )
+
         x_spectr = torch.stack((x_spectr.real, x_spectr.imag), 1).transpose(
             2, 3
         )  # B x 2 x F x T -> B x 2 x T x F
-        return self.layers(x_spectr)  # B x 2 x T x F -> B x C x T x F
+
+        x_more_channels = self.layers(x_spectr)  # B x 2 x T x F -> B x C x T x F
+
+        return x_more_channels
