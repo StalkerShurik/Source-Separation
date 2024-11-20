62f796f6a4efab9366d780a23e48052517280bb0
dikirillov <dikirillov>
2024-11-20 18:24:09 +0000

    init

--- src/model/rtfs_src/separation_network.py
+++ src/model/rtfs_src/separation_network.py
@@ -1,85 +1,11 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from sru import SRU
 
 from .attention_block import Attention2D, GlobalAttention1d
-from .conv_blocks import ConvBlockWithActivation
+from .fusion import CAF
 from .rnn_block import DualPathSRU
 from .rtfs_block import RTFSBlock
-
-
-# CHECKED!
-class CAF(nn.Module):
-    def __init__(self, audio_channels: int, video_channels: int, num_heads=8):
-        super(CAF, self).__init__()
-
-        self.num_heads = num_heads
-
-        self.audio_conv_p1 = ConvBlockWithActivation(
-            in_channels=audio_channels,
-            out_channels=audio_channels,
-            kernel_size=1,
-            is_conv_2d=True,
-            groups=audio_channels,
-            activation_function=torch.nn.Identity,
-        )
-
-        self.audio_conv_p2 = ConvBlockWithActivation(
-            in_channels=audio_channels,
-            out_channels=audio_channels,
-            kernel_size=1,
-            is_conv_2d=True,
-            groups=audio_channels,
-            activation_function=nn.ReLU,
-        )
-
-        self.video_conv_f1 = ConvBlockWithActivation(
-            in_channels=video_channels,
-            out_channels=audio_channels * num_heads,
-            kernel_size=1,
-            is_conv_2d=False,
-            groups=audio_channels,
-            activation_function=torch.nn.Identity,
-        )
-        self.video_conv_f2 = ConvBlockWithActivation(
-            in_channels=video_channels,
-            out_channels=audio_channels,
-            kernel_size=1,
-            is_conv_2d=False,
-            groups=audio_channels,
-            activation_function=torch.nn.Identity,
-        )
-
-    def forward(
-        self, audio_features: torch.Tensor, video_features: torch.Tensor
-    ) -> torch.Tensor:
-        """
-        audio_features: B x Ca x Ta x F
-        video_features: B x Cv x Ta
-        """
-
-        batch_dim, audio_channels_dim, time_dim, features_dim = audio_features.shape
-
-        audio_value = self.audio_conv_p1(audio_features)  # B x Ca x Ta x F
-        audio_gate = self.audio_conv_p2(audio_features)  # B x Ca x Ta x F
-
-        video_attn = self.video_conv_f1(video_features)  # B x (Ca x h) x Tv
-
-        # B x Ca x Tv
-        video_attn = video_attn.reshape(
-            batch_dim, audio_channels_dim, self.num_heads, -1
-        ).mean(dim=2)
-
-        video_attn = torch.softmax(video_attn, -1)
-        video_attn = F.interpolate(video_attn, size=time_dim)
-
-        video_key = self.video_conv_f2(video_features)
-        video_key = torch.nn.functional.interpolate(video_key, size=time_dim)
-
-        return (
-            video_key.unsqueeze(-1) * audio_gate
-            + video_attn.unsqueeze(-1) * audio_value
-        )
 
 
 class SeparationNetwork(nn.Module):
@@ -88,49 +14,65 @@ class SeparationNetwork(nn.Module):
         audio_channels: int,
         video_channels: int,
         rtfs_repeats: int = 4,
+        rtfs_hid_channels: int = 64,
+        dual_path_hidden_channels: int = 32,
+        dual_path_num_layers: int = 4,
+        dual_path_rnn_type: nn.Module = SRU,
+        attention2d_features_dim: int = 64,
+        attention2d_hidden_channels: int = 4,
+        attention2d_num_heads: int = 4,
+        fusion_num_heads: int = 8,
     ) -> None:
         super(SeparationNetwork, self).__init__()
         self.audio_network = RTFSBlock(
             in_channels=audio_channels,
-            hid_channels=64,
+            hid_channels=rtfs_hid_channels,
             kernel_size=4,
             stride=2,
             downsample_layers_count=2,
             is_conv_2d=True,
             attention_layers=nn.Sequential(
                 DualPathSRU(
-                    in_channels=64,
-                    hidden_channels=32,
+                    in_channels=rtfs_hid_channels,
+                    hidden_channels=dual_path_hidden_channels,
                     kernel_size=8,
-                    num_layers=4,
+                    num_layers=dual_path_num_layers,
                     stride=1,
                     bidirectional=True,
                     apply_to_time=False,
+                    rnn_type=dual_path_rnn_type,
                 ),
                 DualPathSRU(
-                    in_channels=64,
-                    hidden_channels=32,
+                    in_channels=rtfs_hid_channels,
+                    hidden_channels=dual_path_hidden_channels,
                     kernel_size=8,
                     stride=1,
-                    num_layers=4,
+                    num_layers=dual_path_num_layers,
                     bidirectional=True,
                     apply_to_time=True,
+                    rnn_type=dual_path_rnn_type,
                 ),
                 Attention2D(
-                    in_channels=64, features_dim=64, hidden_channels=4, num_heads=4
+                    in_channels=rtfs_hid_channels,
+                    features_dim=attention2d_features_dim,
+                    hidden_channels=attention2d_hidden_channels,
+                    num_heads=attention2d_num_heads,
                 ),
             ),
         )
         self.video_network = RTFSBlock(
             in_channels=video_channels,
-            hid_channels=64,
+            hid_channels=rtfs_hid_channels,
             kernel_size=3,
             stride=2,
             downsample_layers_count=4,
             is_conv_2d=False,
             attention_layers=nn.Sequential(
                 GlobalAttention1d(
-                    in_channels=64, kernel_size=3, num_heads=8, dropout=0.1
+                    in_channels=rtfs_hid_channels,
+                    kernel_size=3,
+                    num_heads=8,
+                    dropout=0.1,
                 ),
             ),
         )
@@ -139,7 +81,8 @@ class SeparationNetwork(nn.Module):
         self.caf = CAF(
             audio_channels=audio_channels,
             video_channels=video_channels,
-        )  # CHECK IF IT IS APPROPRIATE CHANNELS
+            num_heads=fusion_num_heads,
+        )
 
     def forward(
         self, audio_features: torch.Tensor, video_features: torch.Tensor
