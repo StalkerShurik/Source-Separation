997852585a123e7745df0af4212615cfc11b6247
TorchRik <storchrik@gmail.com>
2024-11-22 16:07:26 +0300

    model_refactor

--- src/model/rtfs_src/separation_network.py
+++ src/model/rtfs_src/separation_network.py
@@ -1,3 +1,5 @@
+import typing as tp
+
 import torch
 import torch.nn as nn
 from sru import SRU
@@ -8,10 +10,10 @@ from .rnn_block import DualPathSRU
 from .rtfs_block import RTFSBlock
 
 
-def getrnn(rnn_type_str: str):
+def _get_rnn(rnn_type_str: str):
     possible_types = {"SRU": SRU, "GRU": nn.GRU}
     if rnn_type_str not in possible_types.keys():
-        raise Exception("Wronge RNN type")
+        raise Exception("Unsupported RNN type")
     return possible_types[rnn_type_str]
 
 
@@ -20,31 +22,22 @@ class SeparationNetwork(nn.Module):
         self,
         audio_channels: int,
         video_channels: int,
-        rtfs_hid_channels: int,
         rtfs_repeats: int,
-        downsample_rate_2d: int,
-        downsample_rate_1d: int,
         rnn_type_str: str,
-        audio_network_kernel_size: int,
-        audio_network_stride: int,
-        video_network_kernel_size: int,
-        video_network_stride: int,
+        audio_block_params: dict[str, tp.Any],
         dual_path_rnn_params: dict,
-        attention2d_params: dict,
-        attention1d_params: dict,
-        CAF_params: dict,
+        audio_attention_params: dict[str, tp.Any],
+        video_block_params: dict[str, tp.Any],
+        video_attention_params: dict[str, tp.Any],
+        caf_params: dict[str, tp.Any],
     ) -> None:
         super(SeparationNetwork, self).__init__()
 
-        rnn_type = getrnn(rnn_type_str)
+        rnn_type = _get_rnn(rnn_type_str)
 
         self.audio_network = RTFSBlock(
             in_channels=audio_channels,
-            hid_channels=rtfs_hid_channels,
-            kernel_size=audio_network_kernel_size,
-            stride=audio_network_stride,
-            downsample_layers_count=downsample_rate_2d,
-            is_conv_2d=True,
+            **audio_block_params,
             attention_layers=nn.Sequential(
                 DualPathSRU(
                     **dual_path_rnn_params,
@@ -57,27 +50,23 @@ class SeparationNetwork(nn.Module):
                     apply_to_time=True,
                 ),
                 Attention2D(
-                    **attention2d_params,
+                    **audio_attention_params,
                 ),
             ),
         )
         self.video_network = RTFSBlock(
             in_channels=video_channels,
-            hid_channels=rtfs_hid_channels,
-            kernel_size=video_network_kernel_size,
-            stride=video_network_stride,
-            downsample_layers_count=downsample_rate_1d,
-            is_conv_2d=False,
+            **video_block_params,
             attention_layers=nn.Sequential(
                 GlobalAttention1d(
-                    **attention1d_params,
+                    **video_attention_params,
                 ),
             ),
         )
         self.rtfs_repeats = rtfs_repeats
 
         self.caf = CAF(
-            **CAF_params,
+            **caf_params,
             audio_channels=audio_channels,
             video_channels=video_channels,
         )
